c67aac62be914e954e68d114ff2130cd1a386325
frank <frank-surface-wsl>
2024-12-10 09:56:07 +0800

    init remote rocksdb

---
+++ remote-rocksdb/lib/README.md
@@ -0,0 +1,3 @@
+librocksdb.a
+
+version: 6.14.6
---
+++ remote-rocksdb/src/db_server.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include "rocksdb/db.h"
+#include "rocksdb/slice.h"
+#include "rocksdb/options.h"
+
+int main()
+{
+    return 0;
+}
